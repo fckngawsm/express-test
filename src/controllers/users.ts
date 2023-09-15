feb0cec9f15b7ddb3b20d5d4b3d4e8c40916b8b9
kirill <you@example.com>
2023-09-15 23:20:15 +0400

    change the typing for function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { User } from "../models/User";
-export const getAllUsers = (_: Request, res: Response, next: NextFunction) => {
+
+export const getAllUsers: RequestHandler = (_, res, next) => {
   User.findAll({})
     .then((user) => {
       res.send(user);
